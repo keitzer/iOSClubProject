ef1a5fa6ef7c6b48261ac293e1a25ab26815917f
keitzer <ogorek@usc.edu>
2016-02-17 18:41:52 -0500

    Initial Commit

---
+++ iOSClubProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSClubProject
+//
+//  Created by Alex Ogorek on 2/17/16.
+//  Copyright © 2016 iosclub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSClubProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOSClubProject
+//
+//  Created by Alex Ogorek on 2/17/16.
+//  Copyright © 2016 iosclub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
