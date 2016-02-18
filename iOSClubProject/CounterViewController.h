ed548244d1eadee1735ddbfc6f49867448ae8a88
keitzer <ogorek@usc.edu>
2016-02-17 20:02:08 -0500

    counter and total count vc files created

---
+++ iOSClubProject/CounterViewController.h
@@ -0,0 +1,13 @@
+//
+//  CounterViewController.h
+//  iOSClubProject
+//
+//  Created by Alex Ogorek on 2/17/16.
+//  Copyright © 2016 iosclub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CounterViewController : UIViewController
+
+@end
---
+++ iOSClubProject/TotalCountViewController.h
@@ -0,0 +1,13 @@
+//
+//  TotalCountViewController.h
+//  iOSClubProject
+//
+//  Created by Alex Ogorek on 2/17/16.
+//  Copyright © 2016 iosclub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TotalCountViewController : UIViewController
+
+@end
