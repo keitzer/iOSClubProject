e3e69f76815cec567488df6f46339fd2c191085a
keitzer <ogorek@usc.edu>
2016-02-22 19:26:53 -0500

    balance files created

---
+++ iOSClubProject/BalanceViewController.h
@@ -0,0 +1,13 @@
+//
+//  BalanceViewController.h
+//  iOSClubProject
+//
+//  Created by Alex Ogorek on 2/22/16.
+//  Copyright © 2016 iosclub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BalanceViewController : UIViewController
+
+@end
---
+++ iOSClubProject/BalanceViewController.m
@@ -0,0 +1,48 @@
+//
+//  BalanceViewController.m
+//  iOSClubProject
+//
+//  Created by Alex Ogorek on 2/22/16.
+//  Copyright © 2016 iosclub. All rights reserved.
+//
+
+#import "BalanceViewController.h"
+
+@interface BalanceViewController ()
+@property (nonatomic, weak) IBOutlet UIImageView *leftArrowImageView;
+@property (nonatomic, weak) IBOutlet UIImageView *rightArrowImageView;
+@end
+
+@implementation BalanceViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ @property (strong, nonatomic) CMMotionManager *motionManager;
+ self.motionManager = [[CMMotionManager alloc] init];
+ self.motionManager.accelerometerUpdateInterval = .2;
+ [self.motionManager startAccelerometerUpdatesToQueue:[NSOperationQueue currentQueue]
+ withHandler:^(CMAccelerometerData  *accelerometerData, NSError *error) {
+ }];
+ 
+ self.accX.text = [NSString stringWithFormat:@" %.2fg",acceleration.x];
+ */
+
+
+@end
